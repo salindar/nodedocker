1ed77cc46426a5dbf30e0da0906dd2aa7ff8fe88
Rathnayeka <srat0350@corp.sysco.ca>
2018-09-28 14:10:33 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/getHello', (req, res) => {
+	console.log('getHello calling getHello')
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
